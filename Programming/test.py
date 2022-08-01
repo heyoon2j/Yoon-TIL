120b83ca0c601aaa159eaef7836f1ed2bd550dbd
heyoon2j <jyszz1201@gmail.com>
2022-08-01 18:03:54 +0900

    qwer

--- Programming/test.py
+++ Programming/test.py
@@ -1,14 +1,30 @@
 a = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
 print(a[2:8:3])    # [20, 50]
 
-
-# 음수로 마지막 부분부터 연산가능
-print(a[6:-2])    
-
-
-tupleA = (1, 123, 5, 4)
-setB = {1, 2, 3}
+def sortedKey(e):
+    return e[0]
 
 
+def solution(priorities, location):
+    answer = 0
+    sortedList = []
 
+    for i in range(0, len(priorities)):
+        sortedList.append((priorities[i], i + 1))
+
+    print(sortedList)
+    sortedList.sort(key=sortedKey,reverse=True)
+    print(sortedList)
+
+    for i in sortedList:
+        answer += 1
+        if i[1] == location:
+            return answer
+
+    return len(priorities)
+
+
+priorities = [2, 1, 3, 2]
+location = 1
+print(solution(priorities, location))
 
