d232a9f286cfea10305ca235907f9580d018bdce
heyoon2j <jyszz1201@gmail.com>
2022-09-01 01:00:54 +0900

    test

--- Programming/test.py
+++ Programming/test.py
@@ -1,14 +1,81 @@
-a = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
-print(a[2:8:3])    # [20, 50]
+"""
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    trucksOnBridge = []
+    curBridgeWeight = 0
 
-
-# 음수로 마지막 부분부터 연산가능
-print(a[6:-2])    
-
-
-tupleA = (1, 123, 5, 4)
-setB = {1, 2, 3}
+    answer += 1
 
 
 
+    #while len(truck_weights) != 0:
+    while True:
+        answer += 1
+        if len(trucksOnBridge) != 0:
+            for i in trucksOnBridge:
+                i[1] += 1
+            if trucksOnBridge[0][1] == weight:
+                truck = trucksOnBridge.pop(0)
+                curBridgeWeight -= truck[0]
 
+        if (curBridgeWeight + truck_weights[0] <= weight) and len(trucksOnBridge) < bridge_length:
+            truck = truck_weights.pop(0)
+            curBridgeWeight += truck
+            trucksOnBridge.append([truck, 0])
+            
+        if len(truck_weights) == 0 and len(trucksOnBridge) == 0:
+            answer -= 1
+            break
+        print(curBridgeWeight)
+        print(str(trucksOnBridge)+ " / " +str(truck_weights))
+        
+    return answer
+
+
+# 2, 10, [7, 4, 5, 6]
+# 100, 100 [10]
+# 100, 100, [10,10,10,10,10,10,10,10,10,10]
+bridge_length, weight, truck_weights = 100, 100, [10,10,10,10,10,10,10,10,10,10]
+print(solution(bridge_length, weight, truck_weights))
+"""
+
+
+
+from collections import deque
+
+
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    trucksOnBridge = deque()
+    totalWetight = 0
+
+    while True:
+        answer += 1
+
+        if len(trucksOnBridge) != 0:
+            for i in trucksOnBridge:
+                i[1] += 1
+
+            if trucksOnBridge[0][1] == bridge_length:
+                truckOut = trucksOnBridge.popleft()
+                totalWetight -= truckOut[0]
+
+
+        if len(truck_weights) != 0 and len(trucksOnBridge) < bridge_length:
+            if totalWetight + truck_weights[0] <= weight:
+                truckIn = truck_weights.pop(0)
+                trucksOnBridge.append([truckIn, 0])
+                totalWetight += truckIn
+
+
+        print(trucksOnBridge)
+
+        #if len(trucksOnBridge) == 0 and len(truck_weights) == 0:
+        if totalWetight == 0:
+            return answer        
+
+# 2, 10, [7, 4, 5, 6]
+# 100, 100, [10]
+# 100, 100, [10,10,10,10,10,10,10,10,10,10]
+bridge_length, weight, truck_weights =2, 10, [7, 4, 5, 6] 
+print(solution(bridge_length, weight, truck_weights))
